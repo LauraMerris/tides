0cc03582d31b172c35c01a12a964382bdf41e92a
LauraMerris <laura.merris@googlemail.com>
2023-06-05 21:25:19 +0100

    add markers to map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import './App.css';
 import Map from './components/map/Map';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [stations, setStations] = useState([]);
+  const [subStations, setSubStations] = useState([]);
+
+  useEffect(() => {
+    let active = true;
+    setStations(null);
+    fetch("http://environment.data.gov.uk/flood-monitoring/id/stations?type=TideGauge&unitName=m")
+      .then(response => response.json())
+      .then(data => {
+        // this aborts stale requests if multiple requests are in-flight at once.
+        if (active) {
+          setStations(data.items);
+          const subStations = data.items.map(({label, lat, long, stationReference}) => ({label, lat, long, stationReference}) );
+          setSubStations(subStations);
+        }
+      })
+      .catch(e => {
+        console.log(e.message);
+      });
+    return () => {
+      //cleanup function that runs before any subsequent calls
+      //shouldn't need it here because this only runs once?
+      active = false;
+    }
+  },[])
 
   return (
     <div className="App">
       <header className="App-header">
-        LeafletJs
       </header>
       <section id="main">
-        <Map />
+        <Map markers={subStations} />
       </section>
     </div>
   );
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -3,45 +3,28 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import styles from './Map.module.css';
 import 'leaflet-defaulticon-compatibility';
-import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Tooltip, Popup} from 'react-leaflet';
 
-const Map = () => {
+const Map = ({markers}) => {
 
-    /*
-
-    Using react-leaflet instead to avoid needing to manage state and dom updates directly with leaflet
-    useEffect(() => {
-
-        // need this to avoid getting an error but I don't know why it is needed
-        var container = L.DomUtil.get("map");
-        if (container != null) {
-            container._leaflet_id = null;
-        }
-
-        const map = L.map('map').setView([51.505, -0.09], 13);
-        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-
-        var marker = L.marker([51.5, -0.09]).addTo(map);
-        marker.bindPopup("Meanwhile, somewhere near London...");
-        marker.bindTooltip("my tooltip text");
-    },[]);
-    */
+   console.log(markers);
     
     return (
         <div className={styles.mapWrap}>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+            <MapContainer center={[54.5, -3.59]} zoom={6} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {markers.map((elem) => {
+                    return (
+                        <Marker key={elem.stationReference} position={[elem.lat, elem.long]}>
+                            <Tooltip>
+                                {elem.label}
+                            </Tooltip>
+                        </Marker>
+                    )
+                })}         
             </MapContainer>
         </div>
     )
